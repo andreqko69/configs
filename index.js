7294983b82df1db8f9ad5e2b97f53e79b48917db
andreqko <andreqkodev@gmail.com>
2023-06-30 11:11:04 +0300

    Removed webpack config

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-const getReactWebpackConfig = require('./webpack/webpack.config.react.js');
 const prettierConfig = require('./prettier/.prettierrc.js');
 const eslintConfigNode = require('./eslint/.eslintrc.node.js');
 
 module.exports = {
-  getReactWebpackConfig,
   prettierConfig,
   eslintConfigNode
 }
