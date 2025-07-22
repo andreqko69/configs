121c0496f468d48d87922621bd271c9cb8e86eb8
andreqko <andreqkodev@gmail.com>
2025-07-22 19:27:23 +0300

    re-init

--- index.js
+++
@@ -1,7 +0,0 @@
-const prettierConfig = require('./prettier/.prettierrc.js');
-const eslintConfigNode = require('./eslint/.eslintrc.node.js');
-
-module.exports = {
-  prettierConfig,
-  eslintConfigNode
-}
--- prettier/.prettierrc.js
+++ prettier/.prettierrc.js
@@ -1,11 +1,62 @@
 module.exports = {
-  "printWidth": 100,
-  "tabWidth": 2,
-  "singleQuote": true,
-  "trailingComma": "es5",
-  "bracketSpacing": true,
-  "jsxBracketSameLine": false,
-  "semi": true,
-  "useTabs": false,
-  "arrowParens": "avoid"
-}
+  // Line length - balances readability with screen space
+  printWidth: 80,
+
+  // Use 2 spaces for indentation (widely adopted in modern JS)
+  tabWidth: 2,
+  useTabs: false,
+
+  // Add semicolons for statement termination (reduces ASI issues)
+  semi: true,
+
+  // Use single quotes for string literals (popular in modern JS codebases)
+  singleQuote: true,
+
+  // Modern trailing comma style (helps with cleaner git diffs)
+  trailingComma: 'all',
+
+  // Spacing in object literals
+  bracketSpacing: true,
+
+  // JSX formatting
+  jsxSingleQuote: false,
+  bracketSameLine: false,
+
+  // Modern arrow function parentheses handling
+  arrowParens: 'always',
+
+  // Consistent end of line style
+  endOfLine: 'lf',
+
+  // Embedded language formatting
+  embeddedLanguageFormatting: 'auto',
+
+  // Parser options
+  singleAttributePerLine: false,
+
+  // Newer options (as of 2025)
+  quoteProps: 'as-needed',
+  vueIndentScriptAndStyle: false,
+  htmlWhitespaceSensitivity: 'css',
+  proseWrap: 'preserve',
+
+  // Prettier plugins
+  plugins: ['@ianvs/prettier-plugin-sort-imports'],
+  importOrder: [
+    '<BUILTIN_MODULES>',
+    '',
+    '^react$',
+    '<THIRD_PARTY_MODULES>',
+    '',
+    '^@/(.*)$',
+    '^~/(.*)$',
+    '',
+    '^[..]',
+    '^[.]',
+  ],
+  importOrderBuiltinModulesToTop: true,
+  importOrderCaseInsensitive: true,
+  importOrderMergeDuplicateImports: true,
+  importOrderCombineTypeAndValueImports: true,
+  importOrderSortSpecifiers: true,
+};
