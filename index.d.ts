f8d57628cd80319c965151a5f6a05edcb49fbba9
andreqko <andreqkodev@gmail.com>
2025-08-03 23:54:05 +0300

    Create index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,11 @@
+declare module "configs/prettier/base.js" {
+  import { Config } from "prettier";
+  const config: Config;
+  export = config;
+}
+
+declare module "configs/prettier/with-tailwind.js" {
+  import { Config } from "prettier";
+  const config: Config;
+  export = config;
+}
